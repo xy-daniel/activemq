4fa0933ff42f672e45b757d3f38cccafb120ec30
p4m4p <myarctic@163.com>
2020-03-23 22:10:08 +0800

    初始化

---
+++ src/test/java/org/javaboy/activemq/ActivemqApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javaboy.activemq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActivemqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
